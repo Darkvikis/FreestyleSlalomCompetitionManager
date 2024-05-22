8126e2a53f450dab897642082b5730b0bbf8bdd7
Viktor Konupcik <viktorkonupcik@seznam.cz>
2024-05-23 00:19:11 +0200

    refactoring of classic

--- FreestyleSlalomCompetiitonManager.BL/Models/Disciplines/Classic/Classic.cs
+++ FreestyleSlalomCompetiitonManager.BL/Models/Disciplines/Classic/Classic.cs
@@ -13,6 +13,25 @@ namespace FreestyleSlalomCompetitionManager.BL.Models.Disciplines.Classic
 
         public void GenerateRounds(int numberOfPrequalified, int maxNumberOfSkatersInGroup, int numberOfQualificationGroups = 1)
         {
+            ValidateInputParameters(numberOfPrequalified, maxNumberOfSkatersInGroup, numberOfQualificationGroups);
+            InitializeFinalRound();
+            AddPrequalifiedCompetitorsToFinalRound(numberOfPrequalified);
+
+            if (numberOfPrequalified < Competitors.Count)
+            {
+                if (numberOfQualificationGroups > 1)
+                {
+                    GenerateMultipleQualificationRounds(numberOfPrequalified, numberOfQualificationGroups);
+                }
+                else
+                {
+                    GenerateSingleQualificationRound(numberOfPrequalified);
+                }
+            }
+        }
+
+        private void ValidateInputParameters(int numberOfPrequalified, int maxNumberOfSkatersInGroup, int numberOfQualificationGroups)
+        {
             if (numberOfPrequalified <= 0 || maxNumberOfSkatersInGroup <= 0)
             {
                 throw new ArgumentException("Invalid input parameters. Number of prequalified skaters, and maximum number of skaters in a group must be greater than zero.");
@@ -25,66 +44,73 @@ namespace FreestyleSlalomCompetitionManager.BL.Models.Disciplines.Classic
             {
                 throw new ArgumentException("Invalid input parameters. The difference between the maximum number of skaters in a group and the number of prequalified skaters must be greater than or equal to the number of qualification groups.");
             }
+        }
 
+        private void InitializeFinalRound()
+        {
             Rounds.Add(new ClassicRound() { Number = 0, Type = Round.Final });
+        }
 
+        private void AddPrequalifiedCompetitorsToFinalRound(int numberOfPrequalified)
+        {
             for (int i = 0; i < numberOfPrequalified; i++)
             {
                 Rounds.Find(r => r.Type == Round.Final)?.Competitors.Add(Competitors[i]);
             }
+        }
 
-            if (numberOfPrequalified >= Competitors.Count) { return; }
-
-
-            if (numberOfQualificationGroups > 1)
+        private void GenerateMultipleQualificationRounds(int numberOfPrequalified, int numberOfQualificationGroups)
+        {
+            List<ClassicRound> qualifications = new List<ClassicRound>();
+            for (int i = 0; i < numberOfQualificationGroups; i++)
             {
-                List<ClassicRound> Qualifications = [];
-                for (int i = 0; i < numberOfQualificationGroups; i++)
-                {
-                    Qualifications.Add(new ClassicRound() { Number = 1 + i, Type = Round.Qualification });
-                }
-                int roundsCounter = 0;
-                bool addtion = true;
+                qualifications.Add(new ClassicRound() { Number = 1 + i, Type = Round.Qualification });
+            }
 
-                for (int i = numberOfPrequalified; i < Competitors.Count; i++)
+            int roundsCounter = 0;
+            bool addition = true;
+
+            for (int i = numberOfPrequalified; i < Competitors.Count; i++)
+            {
+                if (addition)
                 {
-                    if (addtion)
+                    qualifications[roundsCounter++].Competitors.Add(Competitors[i]);
+                }
+                else
+                {
+                    qualifications[roundsCounter--].Competitors.Add(Competitors[i]);
+                }
+
+                if (roundsCounter >= qualifications.Count || roundsCounter < 0)
+                {
+                    if (addition)
                     {
-                        Qualifications[roundsCounter++].Competitors.Add(Competitors[i]);
+                        addition = false;
+                        roundsCounter--;
                     }
                     else
                     {
-                        Qualifications[roundsCounter--].Competitors.Add(Competitors[i]);
-                    }
-
-                    if (roundsCounter >= Qualifications.Count || roundsCounter < 0)
-                    {
-                        if (addtion)
-                        {
-                            addtion = false;
-                            roundsCounter--;
-                        }
-                        else
-                        {
-                            addtion = true;
-                            roundsCounter++;
-                        }
+                        addition = true;
+                        roundsCounter++;
                     }
                 }
-
-                Rounds.AddRange(Qualifications);
             }
-            else
-            {
-                var qualification = new ClassicRound() { Number = 1, Type = Round.Qualification };
 
-                for (int i = numberOfPrequalified; i < Competitors.Count; i++)
-                {
-                    qualification.Competitors.Add(Competitors[i]);
-                }
-                Rounds.Add(qualification);
-            }
+            Rounds.AddRange(qualifications);
         }
+
+        private void GenerateSingleQualificationRound(int numberOfPrequalified)
+        {
+            var qualification = new ClassicRound() { Number = 1, Type = Round.Qualification };
+
+            for (int i = numberOfPrequalified; i < Competitors.Count; i++)
+            {
+                qualification.Competitors.Add(Competitors[i]);
+            }
+
+            Rounds.Add(qualification);
+        }
+
 
         public override void AssignCompetitors(List<Competitor> skaters)
         {
