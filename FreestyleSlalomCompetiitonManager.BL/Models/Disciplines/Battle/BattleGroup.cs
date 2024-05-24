1c918c578af43019fda3ebdbc21fb545ec043c4b
Darkvikis <48491740+Darkvikis@users.noreply.github.com>
2024-05-25 01:48:02 +0200

    Moved `Battle` class and added `BattleGroup` and `BattleRound` classes
    Moved the `Battle` class to a new namespace `FreestyleSlalomCompetitionManager.BL.Models.Disciplines.Battle`. Added a new class `BattleGroup` with a `Competitors` property, which is an empty dictionary with `Competitor` as the key and `int` as the value. Also added a new class `BattleRound` that inherits from `BaseRound` and has a `Runs` property, which is an empty list of `BattleGroup`.

---
+++ FreestyleSlalomCompetiitonManager.BL/Models/Disciplines/Battle/BattleGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreestyleSlalomCompetitionManager.BL.Models.Disciplines.Battle
+{
+    public class BattleGroup
+    {
+        public Dictionary<Competitor, int> Competitors { get; set; } = [];
+    }
+}
---
+++ FreestyleSlalomCompetiitonManager.BL/Models/Disciplines/Battle/BattleRound.cs
@@ -0,0 +1,14 @@
+﻿using FreestyleSlalomCompetitionManager.BL.Models.Disciplines.Classic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreestyleSlalomCompetitionManager.BL.Models.Disciplines.Battle
+{
+    public class BattleRound : BaseRound
+    {
+        public List<BattleGroup> Runs = [];
+    }
+}
