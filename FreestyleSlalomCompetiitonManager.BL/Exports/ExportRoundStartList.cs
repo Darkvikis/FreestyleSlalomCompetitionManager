cf5a2437e942a7405c408dbcb1958fb5f72de469
Darkvikis <48491740+Darkvikis@users.noreply.github.com>
2024-06-06 12:26:12 +0200

    `Added ExportRoundStartList class for Excel export`
    `Added a new class `ExportRoundStartList` in the `FreestyleSlalomCompetitionManager.BL.Exports` namespace. This class is responsible for exporting the start list of a battle round in a freestyle slalom competition to an Excel file using the `OfficeOpenXml` library. The `ExportBattleRoundStartList` method creates a new Excel worksheet titled "Skater registration data", iterates over the groups in the round, and writes the group number, start list, and competitor details to the worksheet. The method also applies a thin border to the cells containing the competitor details. However, the method does not save the created Excel package to a file or stream, which might be an oversight or the code might be incomplete.`

---
+++ FreestyleSlalomCompetiitonManager.BL/Exports/ExportRoundStartList.cs
@@ -0,0 +1,49 @@
+﻿using FreestyleSlalomCompetitionManager.BL.Models;
+using FreestyleSlalomCompetitionManager.BL.Models.Disciplines.Battle;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreestyleSlalomCompetitionManager.BL.Exports
+{
+    public class ExportRoundStartList
+    {
+        public void ExportBattleRoundStartList(BattleRound round)
+        {
+            // Create a new Excel package
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using ExcelPackage package = new();
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Skater registration data");
+
+            worksheet.Cells[2, 2].Value = $"{round.Type} Battle";
+            int groupNum = 1;
+            foreach (var group in round.Groups)
+            {
+                worksheet.Cells[4, 2].Value = $"Group {groupNum++}";
+                worksheet.Cells[5, 2].Value = "Start list";
+
+                worksheet.Cells[6, 2].Value = "Start number";
+                worksheet.Cells[6, 3].Value = "Name";
+                worksheet.Cells[6, 4].Value = "Country";
+                worksheet.Cells[6, 5].Value = "Rank";
+
+                int row = 7;
+                int skaterNum = 1;
+                foreach (var competitor in group.Competitors)
+                {
+                    worksheet.Cells[row, 2].Value = skaterNum++;
+                    worksheet.Cells[row, 3].Value = $"{competitor.Key.FirstName} {competitor.Key.FamilyName}";
+                    worksheet.Cells[row, 4].Value = competitor.Key.Country;
+
+                    row++;
+                }
+
+                worksheet.Cells[6, 2, row, 5].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                worksheet.Cells[6, 2, row, 5].Style.Border.Left.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+            }
+        }
+    }
+}
